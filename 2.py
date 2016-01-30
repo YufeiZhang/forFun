b9bb7bb138c5b3f6e96d2cecb3f7179efba88495
Yufei Zhang <yufeiGitHub@gmail.com>
2016-01-29 23:17:54 -0700

    build customersMatrix

--- 2.py
+++ 2.py
@@ -4,3 +4,13 @@ calls = open("calls.txt", 'r').read().split('\n')
 request1 = 1 # for testing
 request2 = 1 # 0 -> number of call, 1 -> number of dration
 
+customersMatrix = []
+for i in range(len(customers)-1):
+	customersMatrix.append([])
+	for j in range((len(customers)-1)):
+		customersMatrix[i].append([])
+		customersMatrix[i][j].append(0) #
+		customersMatrix[i][j].append(0) # 
+		customersMatrix[i][j].append(0) # flag
+
+
