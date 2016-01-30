1ec6909b75c9d554da85c308e178e881be970a6d
Yufei Zhang <yufeiGitHub@gmail.com>
2016-01-29 23:17:27 -0700

    start 2.py and hardcode requests

---
+++ 2.py
@@ -0,0 +1,6 @@
+customers = open("customers.txt", 'r').read().split('\n')
+calls = open("calls.txt", 'r').read().split('\n')
+
+request1 = 1 # for testing
+request2 = 1 # 0 -> number of call, 1 -> number of dration
+
