865a2a91732c8ffe1e47ba81fa583f3015ea1767
Yufei Zhang <yufeiGitHub@gmail.com>
2016-01-29 23:13:48 -0700

    get customersList and phoneList

--- 1.py
+++ 1.py
@@ -4,3 +4,14 @@ customers = open("customers.txt", 'r').read().split('\n')
 calls = open("calls.txt", 'r').read().split('\n')
 
 
+customersList, phoneList = [], []
+for i in range(len(customers)-1):
+	customersLine = customers[i].split(';')
+	customersLine.append(0) # number of call
+	customersLine.append(0) # duration in s
+	customersLine.append(0) # due = minutes * rate
+	customersList.append(customersLine)
+	phoneList.append(customersLine[0])
+print(phoneList)
+
+
