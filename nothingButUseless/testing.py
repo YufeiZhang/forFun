fc0e5cb22214335b06f16762eee33db4d5e42eaa
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-03 15:46:28 -0700

    almost done

--- nothingButUseless/testing.py
+++ nothingButUseless/testing.py
@@ -37,38 +37,39 @@ def addTrackToConstraint(ob, name, target):
     return
 
 
-def rotateAsTime(ob, target, index):
-    camera = bpy.data.objects[ob.name]
-    camera.select = True
-    camera.lock_location[2] = True   
+def addKeyFrame(camera, target, currentFrame):
     head = target.pose.bones['Head']
-    camera.rotation_mode = 'XYZ'
     
-    if index % 360 == 0:
+    if currentFrame % 360 == 0:
         camera.location[0] = head.location[0] + 10 * cos(0)
         camera.location[1] = head.location[1] + 10 * sin(0)
-        camera.keyframe_insert('location')
-    elif index % 360 == 90:
+        #camera.keyframe_insert('location')
+    elif currentFrame % 360 == 90:
         camera.location[0] = head.location[0] + 10 * cos(pi/2)
         camera.location[1] = head.location[1] + 10 * sin(pi/2)
-        camera.keyframe_insert('location')
-    elif index % 360 == 180:
+        #camera.keyframe_insert('location')
+    elif currentFrame % 360 == 180:
         camera.location[0] = head.location[0] + 10 * cos(pi)
         camera.location[1] = head.location[1] + 10 * sin(pi)
-        camera.keyframe_insert('location')
-    elif index % 360 == 270:
+        #camera.keyframe_insert('location')
+    elif currentFrame % 360 == 270:
         camera.location[0] = head.location[0] + 10 * cos(3/2*pi)
         camera.location[1] = head.location[1] + 10 * sin(3/2*pi)
-        camera.keyframe_insert('location')
-
+        #camera.keyframe_insert('location')
+    bpy.ops.anim.keyframe_insert(type='Location', confirm_success=True)
+    
     return
 
   
 def run(origin):
-    # delete the existing camera and empty
+    # set the start and end frame
     scn = bpy.context.scene
+    scn.frame_start = 1
+    scn.frame_end = 360
+    
+    # delete all objects excapt armature
     for ob in scn.objects:
-        if ob.type == 'CAMERA':
+        if ob.type != 'ARMATURE':
             bpy.data.objects[ob.name].select = True
             bpy.ops.object.delete()
             
@@ -76,21 +77,37 @@ def run(origin):
     bpy.ops.object.add(type='CAMERA')        
     rot_cam = bpy.context.object
     rot_cam.name = 'rot_cam'
+    rot_cam.lock_location[2] = True
+    rot_cam.rotation_mode = 'XYZ'   
+    rot_cam.select = True
     
     # get the skel_obj
     skel_obj = bpy.data.objects['131_09_60fps']
     
-    # add a copy location constarin
+    # add a copy location constarin to camera
     addCopyLocationConstraint(rot_cam, 'location', skel_obj)
     
-    # add a distance constraint
+    # add a distance constraint to camera
     addDistanceConstraint(rot_cam, 'distance', skel_obj)
                 
-    # add a track constraint    
+    # add a track constraint to camera
     addTrackToConstraint(rot_cam, 'tracking', skel_obj)
     
-    # I want to change the degree every frame
-    rotateAsTime(rot_cam, skel_obj, scn.frame_current)
+    # add keyframe to some specific frame
+    scn.frame_current = 1
+    addKeyFrame(rot_cam, skel_obj, 1)
+    
+    scn.frame_current = 90
+    addKeyFrame(rot_cam, skel_obj, 90)
+    
+    scn.frame_current = 180
+    addKeyFrame(rot_cam, skel_obj, 180)
+    
+    scn.frame_current = 270
+    addKeyFrame(rot_cam, skel_obj, 270)
+    
+    scn.frame_current = 360
+    addKeyFrame(rot_cam, skel_obj, 360)
         
     return
  
